419a9f6e55f013f3540cac39f4523d1f5491d8b4
Towfique Kabir <towfique.pranto@gmail.com>
2026-02-16 20:23:21 +0600

    seperate Delivery Date component

--- client/src/pages/checkout/OrderSummary.jsx
+++ client/src/pages/checkout/OrderSummary.jsx
@@ -1,6 +1,6 @@
-import dayjs from "dayjs";
 import { DeliveryOptions } from "./DeliveryOptions";
 import { CartItemDetails } from "./CartItemDetails";
+import { DeliveryDate } from "./DeliveryDate";
 
 export function OrderSummary({ cart, deliveryOptions }) {
   return (
@@ -15,12 +15,7 @@ export function OrderSummary({ cart, deliveryOptions }) {
 
           return (
             <div key={cartItem.productId} className="cart-item-container">
-              <div className="delivery-date">
-                Delivery date:{" "}
-                {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format(
-                  "dddd, MMMM D",
-                )}
-              </div>
+              <DeliveryDate selectedDeliveryOption={selectedDeliveryOption} />
 
               <div className="cart-item-details-grid">
                 <CartItemDetails cartItem={cartItem} />
