2e4b686aa084eb2c5a29177f13929699f1726ab4
Towfique Kabir <towfique.pranto@gmail.com>
2026-02-16 20:18:22 +0600

    seperate cart item details component

--- client/src/pages/checkout/OrderSummary.jsx
+++ client/src/pages/checkout/OrderSummary.jsx
@@ -1,6 +1,6 @@
-import { formatMoney } from "../../utils/money";
 import dayjs from "dayjs";
 import { DeliveryOptions } from "./DeliveryOptions";
+import { CartItemDetails } from "./CartItemDetails";
 
 export function OrderSummary({ cart, deliveryOptions }) {
   return (
@@ -23,28 +23,7 @@ export function OrderSummary({ cart, deliveryOptions }) {
               </div>
 
               <div className="cart-item-details-grid">
-                <img className="product-image" src={cartItem.product.image} />
-
-                <div className="cart-item-details">
-                  <div className="product-name">{cartItem.product.name}</div>
-                  <div className="product-price">
-                    {formatMoney(cartItem.product.priceCents)}
-                  </div>
-                  <div className="product-quantity">
-                    <span>
-                      Quantity:{" "}
-                      <span className="quantity-label">
-                        {cartItem.quantity}
-                      </span>
-                    </span>
-                    <span className="update-quantity-link link-primary">
-                      Update
-                    </span>
-                    <span className="delete-quantity-link link-primary">
-                      Delete
-                    </span>
-                  </div>
-                </div>
+                <CartItemDetails cartItem={cartItem} />
                 <DeliveryOptions
                   cartItem={cartItem}
                   deliveryOptions={deliveryOptions}
