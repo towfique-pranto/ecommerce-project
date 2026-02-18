1716099233a1c14f142eba125d7d87844fb44289
Towfique Kabir <towfique.pranto@gmail.com>
2026-02-19 02:26:55 +0600

    feat: make update button work

--- client/src/pages/checkout/CartItemDetails.jsx
+++ client/src/pages/checkout/CartItemDetails.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { formatMoney } from "../../utils/money";
+import { useState } from "react";
 
 export function CartItemDetails({ cartItem, loadCart }) {
+  const [updateQuantity, setUpdateQuantity] = useState(false);
+
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
     await loadCart();
@@ -19,9 +22,20 @@ export function CartItemDetails({ cartItem, loadCart }) {
         <div className="product-quantity">
           <span>
             Quantity:{" "}
-            <span className="quantity-label">{cartItem.quantity}</span>
+            {updateQuantity ? (
+              <input className="quantityInput" type="text" />
+            ) : (
+              <span className="quantity-label">{cartItem.quantity}</span>
+            )}
           </span>
-          <span className="update-quantity-link link-primary">Update</span>
+          <span
+            className="update-quantity-link link-primary"
+            onClick={() => {
+              setUpdateQuantity(updateQuantity ? false : true);
+            }}
+          >
+            Update
+          </span>
           <span
             className="delete-quantity-link link-primary"
             onClick={deleteCartItem}
